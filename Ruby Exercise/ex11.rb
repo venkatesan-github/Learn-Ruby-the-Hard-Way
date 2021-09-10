a08aa325ac318b579345a6f6d213f646d06af17d
Venkatesan <76902238+venkatesan-github@users.noreply.github.com>
2021-09-10 21:34:38 +0530

    Add files via upload

---
+++ Ruby Exercise/ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall you are?"
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
---
+++ Ruby Exercise/ex14.rb
@@ -0,0 +1,27 @@
+user_name = ARGV.first
+prompt = '.....'
+
+puts "Hi #{user_name}."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user_name}? "
+puts prompt
+likes = $stdin.gets.chomp
+puts prompt
+
+
+puts "Where do you live #{user_name}? "
+puts prompt
+lives = $stdin.gets.chomp
+puts prompt
+
+
+puts "What kind of computer do you have? ", prompt
+computer = $stdin.gets.chomp
+puts prompt
+
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}. Not sure where that is.
+And you have a #{computer} computer. Nice.
+"""
